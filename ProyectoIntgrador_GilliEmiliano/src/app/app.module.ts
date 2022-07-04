7ec7386ee511208256970133cfcfe0925227fbb0
emilianog1 <95345781+emilianog1@users.noreply.github.com>
2022-07-04 16:04:42 -0300

    Realizacion del Header

--- ProyectoIntgrador_GilliEmiliano/src/app/app.module.ts
+++ ProyectoIntgrador_GilliEmiliano/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoArgProgComponent } from './componentes/logo-arg-prog/logo-arg-prog.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
